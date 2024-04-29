1b27b441006dfd5b0e3d91fe254e3b17de58bf22
jhodges <jhodges@stayinfront.com>
2024-04-30 11:53:54 +1200

    understanding of layers

--- src/ProductClassificationNet.py
+++ src/ProductClassificationNet.py
@@ -37,29 +37,33 @@ class Net(nn.Module):
 
 
 class Net2(nn.Module):
-    def __init__(self, num_classes=10):
+    def __init__(self, num_classes=10, image_dim=32):
         super(Net2, self).__init__()
 
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1), # output size = ((input_size-kernel_size+2*padding)/stride) + 1
+            # O = ((32 - 3 + 2 * 1)/1  + 1) = 32, depth (out channels) = 32, so dimensions = 32 * 32 * 32
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2), # halves output size
+            # O = (32 - 2 + 2 * 1 )/2) = (16) * 16 * 32
+            nn.Conv2d(32, 64, kernel_size=3, padding=1), # increases depth to 64
+            # 16 * 16 * 64
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
+            # O = (16 - 2 + 2 * 1) / 2) = (8) * 8 * 64
         )
 
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
-            nn.Linear(64 * 8 * 8, 512),
+            nn.Linear(64 * int(image_dim/4)**2, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
